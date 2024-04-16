9b359f87ebf93fa9efa6cf7e1e7bb5efe771176b
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-04-16 14:18:56 +0200

    exercise

---
+++ javascript/main.js
@@ -0,0 +1,7 @@
+let nome = prompt("Inserisci Nome");
+
+let cognome = prompt("Inserisci Cognome");
+
+let colore = prompt("Inserisci Colore");
+
+document.getElementById("password").innerHTML = nome + cognome + colore + "#24";
